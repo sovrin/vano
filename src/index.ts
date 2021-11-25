cf9c658379c5426b870e8efae83d4c0e2ceaf454
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2021-11-25 12:54:26 +0100

    feat, export types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import database from './database';
-
 /**
  * User: Oleg Kamlowski <oleg.kamlowski@thomann.de>
  * Date: 01.10.2020
  * Time: 00:02
  */
-export default database;
+export {default} from './database';
 export * from './adapters';
+export type {Adapter, Collection, Config, Database, Data, Entry, Query} from './types';
